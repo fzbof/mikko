821f544b07535ad14de75389b760db8b24b73ac6
David Suys <suysdavid@gmail.com>
2017-01-20 22:14:54 +0100

    Add bonus calculation

--- src/PayrollBundle/Service/PaydayService.php
+++ src/PayrollBundle/Service/PaydayService.php
@@ -4,6 +4,7 @@ namespace PayrollBundle\Service;
 
 /**
  * Class PaydayService
+ *
  * @package PayrollBundle\Service
  */
 class PaydayService
@@ -14,9 +15,20 @@ class PaydayService
     private $weekendDays = [0, 6];
 
     /**
+     * @var int Day of the next month to pay the bonuses
+     */
+    private $bonusDay = 15;
+
+    /**
+     * @var int Day of the week to pay the bonuses in case bonusDay falls in
+     *      the weekend
+     */
+    private $weekendBonusWeekday = 3;
+
+    /**
      * Calculates the pay day of the salary for a given year and month
      *
-     * @param int $year Year in YYYY format
+     * @param int $year  Year in YYYY format
      * @param int $month Month in MM format
      *
      * @return \DateTime
@@ -28,6 +40,34 @@ class PaydayService
 
         while (in_array($payDay->format("w"), $this->weekendDays)) {
             $payDay->sub(new \DateInterval("P1D"));
+        }
+
+        return $payDay;
+    }
+
+    /**
+     * Calculates the pay day of the bonus for a given year and month
+     *
+     * @param int $year  Year in YYYY format
+     * @param int $month Month in MM format
+     *
+     * @return \DateTime
+     */
+    public function calculateBonusPayday(int $year, int $month)
+    {
+        $payDay = new \DateTime();
+        $payDay->setDate($year, $month + 1, $this->bonusDay);
+
+        $dayOfWeek = (int)$payDay->format("w");
+        if (in_array($dayOfWeek, $this->weekendDays)) {
+            $payDay->add(
+              new \DateInterval(
+                sprintf(
+                  "P%dD",
+                  (7 - $dayOfWeek + $this->weekendBonusWeekday) % 7
+                )
+              )
+            );
         }
 
         return $payDay;
