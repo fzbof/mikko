06d252894fee392051652858b4b81c7a73f72481
David Suys <suysdavid@gmail.com>
2017-01-20 22:27:54 +0100

    Add tests for bonus calculation

--- tests/PayrollBundle/Service/PaydayServiceTest.php
+++ tests/PayrollBundle/Service/PaydayServiceTest.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace tests\PayrollBundle\Service;
-
 
 use PayrollBundle\Service\PaydayService;
 
@@ -17,12 +15,15 @@ class PaydayServiceTest extends \PHPUnit_Framework_TestCase
     public function testSalaryPaydayFallsOnLastDayOfMonth($year, $month)
     {
         $paydayService = new PaydayService();
-        $salaryPayday = $paydayService->calculateSalaryPayday($year, $month);
+        $salaryPayday  = $paydayService->calculateSalaryPayday($year, $month);
 
         $referenceDay = new \DateTime();
         $referenceDay->setDate($year, $month, 1);
 
-        $this->assertEquals($salaryPayday->format("Y-m-d"), $referenceDay->format("Y-m-t"));
+        $this->assertEquals(
+          $salaryPayday->format("Y-m-d"),
+          $referenceDay->format("Y-m-t")
+        );
     }
 
     /**
@@ -31,29 +32,88 @@ class PaydayServiceTest extends \PHPUnit_Framework_TestCase
      * @param int $year
      * @param int $month
      */
-    public function testSalaryPaydayFallsOnLastFridayOfMonthIfItEndsInAWeekend($year, $month)
-    {
+    public function testSalaryPaydayFallsOnLastFridayOfMonthIfItEndsInAWeekend(
+      $year,
+      $month
+    ) {
         $paydayService = new PaydayService();
-        $salaryPayday = $paydayService->calculateSalaryPayday($year, $month);
+        $salaryPayday  = $paydayService->calculateSalaryPayday($year, $month);
 
         $referenceDay = new \DateTime();
         $referenceDay->setDate($year, $month, 1);
 
-        $this->assertEquals($salaryPayday->format("w"), 5);
+        $this->assertEquals($salaryPayday->format("w"), 5, "on a friday");
         $this->assertEquals(
           $salaryPayday->format("m"),
-          $referenceDay->format("m")
+          $referenceDay->format("m"),
+          "same month"
         );
         $this->assertNotEquals(
           $salaryPayday->add(new \DateInterval('P1W'))->format("m"),
-          $referenceDay->format("m")
+          $referenceDay->format("m"),
+          "last week of the month"
+        );
+    }
+
+    /**
+     * @dataProvider nextFifteenthIsInWeek
+     *
+     * @param int $year
+     * @param int $month
+     */
+    public function testBonusPaydayFallsOnNextFifteenth($year, $month)
+    {
+        $paydayService = new PaydayService();
+        $bonusPayday   = $paydayService->calculateBonusPayday($year, $month);
+
+        $referenceDay = new \DateTime();
+        $referenceDay->setDate($year, $month + 1, 15);
+
+        $this->assertEquals(
+          $bonusPayday->format("Y-m-d"),
+          $referenceDay->format("Y-m-d")
+        );
+    }
+
+    /**
+     * @dataProvider nextFifteenthIsInWeekend
+     *
+     * @param int $year
+     * @param int $month
+     */
+    public function testBonusPaydayFallsOnFirstWednesdayAfterFifteenth(
+      $year,
+      $month
+    ) {
+        $paydayService = new PaydayService();
+        $bonusPayday   = $paydayService->calculateBonusPayday($year, $month);
+
+        $referenceDay = new \DateTime();
+        $referenceDay->setDate($year, $month + 1, 15);
+
+        $this->assertEquals($bonusPayday->format("w"), 3, "on a wednesday");
+        $this->assertEquals(
+          $bonusPayday->format("m"),
+          $month + 1,
+          "of next month"
+        );
+        $this->assertLessThan(
+          $bonusPayday,
+          $referenceDay,
+          "after the fifteenth"
+        );
+        $this->assertGreaterThan(
+          $bonusPayday,
+          $referenceDay->add(new \DateInterval('P1W')),
+          "in the week after the fifteenth"
         );
     }
 
     /**
      * @return array
      */
-    public function monthEndsInWeek(){
+    public function monthEndsInWeek()
+    {
         return [
           [2017, 1],
           [2017, 2],
@@ -68,10 +128,36 @@ class PaydayServiceTest extends \PHPUnit_Framework_TestCase
     /**
      * @return array
      */
-    public function monthEndsInWeekend(){
+    public function monthEndsInWeekend()
+    {
         return [
           [2017, 4],
           [2017, 9],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function nextFifteenthIsInWeek()
+    {
+        return [
+          [2017, 1],
+          [2017, 2],
+          [2017, 4],
+          [2017, 5],
+          [2017, 7],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function nextFifteenthIsInWeekend()
+    {
+        return [
+          [2017, 3],
+          [2017, 6],
         ];
     }
 }
